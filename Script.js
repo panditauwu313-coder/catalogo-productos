a67cc5d20cd4e51fcfa1e856b2470f09d830ad88
Hector Khotan <Panditauwu313@gmail.com>
2026-04-01 03:15:10 -0600

    Agregué nuevos productos

--- Script.js
+++ Script.js
@@ -4,3 +4,25 @@ document.querySelectorAll('.product-card button').forEach(button => {
         alert('Producto añadido al carrito');
     });
 });
+
+document.getElementById("pedidoForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  const nombre = document.getElementById("nombre").value;
+  const producto = document.getElementById("producto").value;
+  const cantidad = document.getElementById("cantidad").value;
+  const notas = document.getElementById("notas").value;
+
+  const pedido = {
+    nombre,
+    producto,
+    cantidad,
+    notas
+  };
+
+  console.log("Pedido:", pedido);
+
+  alert("Pedido enviado (solo prueba)");
+
+  this.reset();
+});

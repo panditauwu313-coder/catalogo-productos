6d1d965716338e4c946fde4acb725104e77f441b
Hector Khotan <Panditauwu313@gmail.com>
2026-04-05 21:27:31 -0600

    mejora en la interfaz de admin

--- firebase.js
+++ firebase.js
@@ -1,17 +1,35 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getFirestore, collection, addDoc, getDocs, updateDoc, doc, onSnapshot } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  updateDoc,
+  deleteDoc,
+  doc,
+  onSnapshot
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD8Q5INY_EfTlyKp_vlnJZP9xWeqH_QhBg",
- authDomain: "catalogo-farid.firebaseapp.com",
+  authDomain: "catalogo-farid.firebaseapp.com",
   projectId: "catalogo-farid",
-  storageBucket: "catalogo-farid.firebasestorage.app",
+  storageBucket: "catalogo-farid.appspot.com",
   messagingSenderId: "603623739227",
-  appId: "1:603623739227:web:9367d301443e5978a1147e",
-  measurementId: "G-E95G99KCD3"
+  appId: "1:603623739227:web:9367d301443e5978a1147e"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-export { db, collection, addDoc, getDocs, updateDoc, doc, onSnapshot };
+export {
+  db,
+  collection,
+  addDoc,
+  getDocs,
+  updateDoc,
+  deleteDoc, // 🔥 IMPORTANTE
+  doc,
+  onSnapshot
+};
